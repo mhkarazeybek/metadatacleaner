ae0e44443f39340e4bf07f33e77f94f1ddb5baa9
mhkarazeybek <mhkarazeybek@gmail.com>
2023-11-03 02:10:12 +0300

    added doc file props

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import PyPDF2
 from docx import Document
 from openpyxl import load_workbook
 import argparse
+from win32com import client
 
 def clear_metadata_image(image_path):
     try:
@@ -13,7 +14,7 @@ def clear_metadata_image(image_path):
         image_without_exif.putdata(data)
         image_without_exif.save(image_path)
     except Exception as e:
-        print(f"Error 1: {e}")
+        print(f"Error Image: {e}")
 
 def clear_metadata_pdf(pdf_path):
     try:
@@ -28,20 +29,45 @@ def clear_metadata_pdf(pdf_path):
         with open(pdf_path, 'wb') as new_file:
             writer.write(new_file)
     except Exception as e:
-        print(f"Error 2: {e}")
+        print(f"Error PDF: {e}")
 
 def clear_metadata_word(doc_path):
     try:
         doc = Document(doc_path)
-        doc.core_properties.authors = ""
+        doc.core_properties.author = ""
         doc.core_properties.title = ""
         doc.core_properties.subject = ""
         doc.core_properties.creator = ""
         doc.core_properties.keywords = ""
         doc.core_properties.last_modified_by = ""
+        doc.core_properties.language = ""
+        doc.core_properties.category = ""
+        doc.core_properties.company = ""
+        doc.core_properties.revision = 1
+        doc.core_properties.program_name = ""
+
         doc.save(doc_path)
     except Exception as e:
-        print(f"Error 3: {e}")
+        print(f"Error Docx")
+
+def clear_metadata_doc(doc_path):
+    try:
+        word = client.Dispatch("Word.Application")
+        word.Visible = False
+        doc = word.Documents.Open(doc_path)
+        props = ["Title","Subject","Author","Keywords","Comments","Template","Last Author","Application Name","Category","Format","Manager","Company","Hyperlink Base","Content status","Content type"]
+        for p in props:
+            doc.BuiltInDocumentProperties(p).Value = ""
+        doc.BuiltInDocumentProperties("Revision").Value = 1
+        # TODO: Last saved by
+
+        doc.Save()
+    except Exception as e:
+        print(f"Error Doc: {e}")
+
+    finally:
+        doc.Close()
+        word.Quit()
 
 def clear_metadata_excel(excel_path):
     try:
@@ -56,9 +82,10 @@ def clear_metadata_excel(excel_path):
         props.program = ""
         props.comments = ""
         props.company = ""
+        props.revision = 0
         wb.save(excel_path)
     except Exception as e:
-        print(f"Error 4: {e}")
+        print(f"Error 5")
 
 def clear_metadata_folder(folder_path):
     for root, dirs, files in os.walk(folder_path):
@@ -70,6 +97,8 @@ def clear_metadata_folder(folder_path):
                 clear_metadata_pdf(file_path)
             elif file.lower().endswith('.docx'):
                 clear_metadata_word(file_path)
+            elif file.lower().endswith('doc'):
+                clear_metadata_doc(file_path)
             elif file.lower().endswith(('.xlsx', '.xls')):
                 clear_metadata_excel(file_path)
             else:
@@ -78,7 +107,7 @@ def clear_metadata_folder(folder_path):
 
 def main():
     parser = argparse.ArgumentParser(description="Meta Data Cleaner")
-    parser.add_argument("folder_path",type=str, help="Clean This Folder", default="test")
+    parser.add_argument("folder_path",type=str, help="Clean This Folder", default="/test")
     args = parser.parse_args()
 
     if os.path.isdir(args.folder_path):
